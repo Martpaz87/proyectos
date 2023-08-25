4b489ca90b62fe0b2076cfdd51132b25e5fd0d2f
unknown <marypazch1@gmail.com>
2023-08-25 11:30:09 -0300

    calculo de sueldo promedio anual

---
+++ proyecto_1.py
@@ -0,0 +1,13 @@
+#Empleado Juan. Calcular sueldo promedio.
+"""
+def Sueldo_promedio(m1, m2 , m3, m4, m5, m6, m7, m8, m9, m10, m11, m12):
+    suma = m1 + m2 + m3 + m4 + m5 + m6 +m7 + m8 + m9 + m10 + m11 + m12
+    meses = [m1, m2 , m3, m4, m5, m6, m7, m8, m9, m10, m11, m12]
+    print("El sueldo promedio anual del empleado Juan es:", suma/meses.len())
+    
+Sueldo_promedio(300, 300, 300, 300, 300, 300, 500, 500, 500, 500, 700, 700)
+"""
+sueldos = [300, 300, 300, 300, 300, 300, 500, 500, 500, 500, 700, 700]
+suma_sueldos = sueldos [0] + sueldos [1] + sueldos [2] + sueldos [3] + sueldos [4] + sueldos [5] + sueldos [6] + sueldos [7] + sueldos [8] + sueldos [9] + sueldos [10] + sueldos [11]
+meses = len(sueldos)
+print("El promedio de sueldo anual de Juan es: ", suma_sueldos/meses)
