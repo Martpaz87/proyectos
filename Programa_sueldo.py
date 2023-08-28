fec3869977a26e1d721de00143da3263e6af58d6
unknown <marypazch1@gmail.com>
2023-08-28 10:09:53 -0300

    condicionales y fin del programa

--- Programa_sueldo.py
+++ Programa_sueldo.py
@@ -1,16 +1,25 @@
 #calcular sueldo promedio del empleado Juan.
 
-
-Sueldos_juan = [300, 300, 300, 300, 300, 300, 500, 500, 500, 500, 700, 700]
-suma = 0
-meses = len(Sueldos_juan)
 def Sueldo_promedio(Sueldos_juan):
-    
+
+    meses = len(Sueldos_juan)
+    suma = 0
     for n in Sueldos_juan:
         suma = suma + n
-        return suma
-promedio = suma/meses
-print("El sueldo promedio anual del empleado Juan es:", promedio) 
+        
+    promedio = suma/meses
+    return promedio
 
+
+Sueldos_juan = [300, 300, 300, 300, 300, 300, 500, 500, 500, 500, 700, 700]
+promedio_calculado = Sueldo_promedio(Sueldos_juan)
     
+print("El sueldo promedio anual del empleado Juan es:", promedio_calculado ) 
+
+if promedio_calculado < 300 :
+    print("El empleado/a cobra un sueldo bajo.")
+if promedio_calculado > 300 and promedio_calculado < 900 :
+    print("El sueldo percibido esta dentro e los parametros normales. ")
+else :
+    print("El sueldo percibido es mayor de lo normal. ")
 
