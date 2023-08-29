2d36c2dccfad2c60f2110079ec7010a1ce1f2646
unknown <marypazch1@gmail.com>
2023-08-29 09:37:58 -0300

    correccion signos

--- Programa_sueldo.py
+++ Programa_sueldo.py
@@ -16,9 +16,9 @@ promedio_calculado = sueldo_promedio(sueldos_juan)
     
 print("El sueldo promedio anual del empleado Juan es:", promedio_calculado ) 
 #clasificar dicho sueldo segun corresponda en referencia a los valores dados.
-if promedio_calculado < 300 :
+if promedio_calculado <= 300 :
     print("El empleado/a cobra un sueldo bajo.")
-if promedio_calculado > 300 and promedio_calculado < 900 :
+if promedio_calculado >= 300 and promedio_calculado <= 900 :
     print("El sueldo percibido esta dentro e los parametros normales. ")
 else :
     print("El sueldo percibido es mayor de lo normal. ")
